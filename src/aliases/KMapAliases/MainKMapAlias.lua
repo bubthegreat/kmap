bb9ade31ae0a0a1ff6a62923715a4d9ba9ff5afc
cable2999 <cable2999@users.noreply.github.com>
2025-12-26 12:06:10 -0600

    Most basic functions for main alias.

--- src/aliases/KMapAliases/MainKMapAlias.lua
+++ src/aliases/KMapAliases/MainKMapAlias.lua
@@ -4,6 +4,24 @@ local kmaparray = string.split(kmapcmd)
 
 local object = kmaparray[1]
 
+-- object is map for these essentially, so these are more like actions
+if object == "start" then
+    map.start_mapping(args)
+elseif object == "stop" then
+    raiseEvent("mapStop")
+    raiseEvent("sysSpeedwalkStopped")
+elseif object == "help" then
+    local args = table.concat(kmaparray, " ", 2, table.size(kmaparray))
+    map.show_help(args)
+elseif object == "save" then
+    if saveMap(getMudletHomeDir() .. "/kmapdata/map.dat") and saveJsonMap(getMudletHomeDir() .. "/kmapdata/kmap.json") then
+        map.log("Map saved.\n", "INFO")
+    else
+        map.log("Error.  Map NOT saved.\n", "ERROR")
+    end
+				
+end
+
 if object == "area" then
     local action = kmaparray[2]
     local args = table.concat(kmaparray, " ", 3, table.size(kmaparray))
@@ -15,11 +33,6 @@ if object == "area" then
     elseif action == "list" then
         map.echoAreaList()
     end
-    if action == "start" then
-        map.start_mapping(args)
-    end
-    if action == "stop" then
-        raiseEvent("mapStop")
-        raiseEvent("sysSpeedwalkStopped")
-    end
 end
+    
+
